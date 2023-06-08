c5c5778cdd89a46bf56a3ffd74197b6c959e15bd
mayatrajay <jaymayatra4@gmail.com>
2023-06-08 12:51:01 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Introduction 👋
+
+Next Portfolio is built using Next.js and Tailwind CSS for a modern design and rapid development. TypeScript is utilized for code clarity and safety. Additionally, Firebase is being integrated for backend services such as realtime-database. The result is a dynamic and functional portfolio website that showcases the developer's skills and experience.
+
+## Tech Stack 🛠️
+
+- [Next.js](https://nextjs.org)
+- [TypeScript](https://www.typescriptlang.org)
+- [Tailwind CSS](https://tailwindcss.com)
+- [Firebase](https://firebase.google.com)
+- [SendGrid](https://sendgrid.com)
+- [Framer Motion](https://www.framer.com/motion)
---
+++ app/head.tsx
@@ -0,0 +1,21 @@
+export default function Head() {
+	return (
+	  <>
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+		<meta name="description" content="An experienced Web Developer" />
+		<meta name="keywords" content="Jay Mayatra, portfolio, jay, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+		<meta property="og:site_name" content="Jay Mayatra's Portfolio" />
+		<meta property="og:title" content="Portfolio | Jay Mayatra - Full Stack Developer" />
+		<meta property="og:description"
+		  content="Ane experienced Web Developer" />
+		
+		<meta name="author" content="Jay Mayatra" />
+		<meta name="language" content="English" />
+		<meta name="application-name" content="Portfolio | Jay Mayatra - Full Stack Developer" />
+		<meta name="apple-mobile-web-app-title" content="Portfolio | Jay Mayatra - Full Stack Developer" />
+		<link rel="icon" href="/favicon.ico" />
+		<title>Portfolio | Jay Mayatra - Full Stack Developer</title>
+	  </>
+	)
+  }
+  
